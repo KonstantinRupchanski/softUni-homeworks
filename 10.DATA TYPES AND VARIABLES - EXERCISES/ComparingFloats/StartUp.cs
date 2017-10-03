7e28cf5ae5a13c1d34696a37bbd42083def7b205
KonstantinRupchanski <konstantin.rupchanski@gmail.com>
2017-10-03 12:05:33 +0300

    03/10/2017-12:05

---
+++ 10.DATA TYPES AND VARIABLES - EXERCISES/ComparingFloats/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace ComparingFloats
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            decimal firstNum = decimal.Parse(Console.ReadLine());
+            decimal secondNum = decimal.Parse(Console.ReadLine());
+            decimal difference = Math.Abs(firstNum - secondNum);
+            decimal eps = 0.000001M;
+            if (difference >= 0)
+            {
+                if (difference < eps)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+            }
+            else if (difference < 0)
+            {
+                if (difference < eps)
+                {
+                    Console.WriteLine("False");
+                }
+                else
+                {
+                    Console.WriteLine("True");
+                }
+            }
+        }
+    }
+}
