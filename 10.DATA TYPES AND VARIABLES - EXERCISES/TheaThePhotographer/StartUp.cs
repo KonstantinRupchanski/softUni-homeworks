846b37df34c40deb84303fd2675802b6ee3f893a
KonstantinRupchanski <konstantin.rupchanski@gmail.com>
2017-10-03 16:37:42 +0300

    03/10/2017-16:37

---
+++ 10.DATA TYPES AND VARIABLES - EXERCISES/TheaThePhotographer/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace TheaThePhotographer
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            long pictures = long.Parse(Console.ReadLine());
+            long secondsPerPicture = long.Parse(Console.ReadLine());
+            long filterPercent = long.Parse(Console.ReadLine());
+            long uploadTimePerPicture = long.Parse(Console.ReadLine());
+            long filterSeconds = pictures * secondsPerPicture;
+            long filteredPics = (long)Math.Ceiling(filterPercent * pictures / 100.0);
+            long uploadSeconds = filteredPics * uploadTimePerPicture;
+            long totalSeconds = filterSeconds + uploadSeconds;
+            TimeSpan time = TimeSpan.FromSeconds(totalSeconds);
+            string result = time.ToString(@"d\:hh\:mm\:ss");
+            Console.WriteLine(result);
+        }
+    }
+}
