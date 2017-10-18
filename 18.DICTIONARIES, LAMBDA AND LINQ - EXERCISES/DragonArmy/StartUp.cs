e8855dfda878abc6d5c4fa73a9f9a0523633b949
KonstantinRupchanski <konstantin.rupchanski@gmail.com>
2017-10-18 22:07:43 +0300

    181020172207

--- 18.DICTIONARIES, LAMBDA AND LINQ - EXERCISES/DragonArmy/StartUp.cs
+++ 18.DICTIONARIES, LAMBDA AND LINQ - EXERCISES/DragonArmy/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DragonArmy
 {
@@ -6,7 +8,67 @@ namespace DragonArmy
     {
         public static void Main()
         {
+            Dictionary<string, List<Dragon>> dragons = new Dictionary<string, List<Dragon>>();
 
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split().ToArray();
+                string type = line[0];
+                string name = line[1];
+                int dmg = line[2] != "null" ? int.Parse(line[2]) : 45;
+                int hp = line[3] != "null" ? int.Parse(line[3]) : 250;
+                int armor = line[4] != "null" ? int.Parse(line[4]) : 10;
+
+                if (!dragons.ContainsKey(type))
+                {
+                    dragons.Add(type, new List<Dragon>());
+                }
+
+                if (!dragons[type].Where(x => x.Name == name).Any())
+                {
+                    dragons[type].Add(new Dragon(dmg, hp, armor, name));
+                }
+
+                dragons[type].Where(x => x.Name == name).First().Armor = armor;
+                dragons[type].Where(x => x.Name == name).First().Damage = dmg;
+                dragons[type].Where(x => x.Name == name).First().Health = hp;
+
+
+            }
+
+            foreach (var type in dragons)
+            {
+                double dmgAv = (double)type.Value.Select(x => x.Damage).Sum() / (double)type.Value.Count();
+                double hpAv = (double)type.Value.Select(x => x.Health).Sum() / (double)type.Value.Count();
+                double armAv = (double)type.Value.Select(x => x.Armor).Sum() / (double)type.Value.Count();
+
+                Console.WriteLine($"{type.Key}::({dmgAv:f2}/{hpAv:f2}/{armAv:f2})");
+                foreach (var dragon in type.Value.OrderBy(x => x.Name))
+                {
+                    Console.WriteLine($"-{dragon.Name} -> damage: {dragon.Damage}, health: {dragon.Health}, armor: {dragon.Armor}");
+                }
+            }
+        }
+    }
+
+    class Dragon
+    {
+        public int Damage = 45;
+
+        public int Health = 250;
+
+        public int Armor = 10;
+
+        public string Name { get; set; }
+
+        public Dragon(int dmg, int hp, int armor, string name)
+        {
+            this.Damage = dmg;
+            this.Health = hp;
+            this.Armor = armor;
+            this.Name = name;
         }
     }
 }
--- 18.DICTIONARIES, LAMBDA AND LINQ - EXERCISES/PopulationCounter/StartUp.cs
+++ 18.DICTIONARIES, LAMBDA AND LINQ - EXERCISES/PopulationCounter/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PopulationCounter
 {
@@ -6,7 +8,43 @@ namespace PopulationCounter
     {
         public static void Main()
         {
+            string[] input = Console.ReadLine().Split('|').ToArray();
+            Dictionary<string, Dictionary<string, long>> populations = new Dictionary<string, Dictionary<string, long>>();
 
+            while (input[0] != "report")
+            {
+                string city = input[0];
+                string country = input[1];
+                long population = long.Parse(input[2]);
+                Dictionary<string, long> cityPopulation = new Dictionary<string, long>();
+
+                if (!populations.ContainsKey(country))
+                {
+                    cityPopulation[city] = population;
+                    populations.Add(country, cityPopulation);
+                }
+                else
+                {
+                    cityPopulation = populations[country];
+                    if (cityPopulation.ContainsKey(city))
+                    {
+                        cityPopulation[city] += population;
+                    }
+                    else
+                    {
+                        cityPopulation.Add(city, population);
+                        populations[country] = cityPopulation;
+                    }
+                }
+                input = Console.ReadLine().Split('|').ToArray();
+            }
+            foreach (var state in populations.OrderByDescending(x => x.Value.Sum(y => y.Value)))
+            {
+                List<long> sumOfTowns = state.Value.Select(x => x.Value).ToList();
+                Console.WriteLine($"{state.Key} (total population: {sumOfTowns.Sum()})");
+
+                Console.Write($"=>{string.Join("=>", state.Value.OrderByDescending(x => x.Value).Select(x => $"{x.Key}: {x.Value}\r\n"))}");
+            }
         }
     }
 }
